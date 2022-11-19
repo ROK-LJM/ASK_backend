ea9de78299760a78751399d0263654e5226ef16b
ROK-LJM <fkrtms23@gmail.com>
2022-11-19 18:13:10 +0900

    test commit2
    test commit2

--- ASK_project/src/main/java/com/ask/ask_project/controller/BoardController.java
+++ ASK_project/src/main/java/com/ask/ask_project/controller/BoardController.java
@@ -2,11 +2,9 @@ package com.ask.ask_project.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @Controller
 public class BoardController {
 
